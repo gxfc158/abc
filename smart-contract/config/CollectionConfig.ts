cc4da67322acdfef4e40dfbb63338487b21ea8e2
zg888 <zg888@example.com>
2023-04-17 16:55:03 +0800

    PiApeClub

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,11 +9,11 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'PiDAOClub1',
-  tokenName: 'Pi DAO Club',
+  contractName: 'PiAPEClub',
+  tokenName: 'Pi Ape Club',
   tokenSymbol: 'Pi NFT',
-  hiddenMetadataUri: 'ipfs://Qme4cZU1QZsLgvqhZhyoB19Z7zJydBKrTp9tNSGatf3Lwc/1.json',
-  maxSupply: 8888,
+  hiddenMetadataUri: 'ipfs://bafybeidt4amrboa67gk5wqaewbeyrtdfpkyevjzhz3ltjei4sarp2z65ay/1.json',
+  maxSupply: 5000,
   whitelistSale: {
     price: 0.1,
     maxMintAmountPerTx: 1,
@@ -24,9 +24,9 @@ const CollectionConfig: CollectionConfigInterface = {
   },
   publicSale: {
     price: 0.5,
-    maxMintAmountPerTx: 1000,
+    maxMintAmountPerTx: 5000,
   },
-  contractAddress:'0xd14dBa13A56Ba0bB454BF256E7207f8e0B2376E6',
+  contractAddress:'0xDd782323bF8ef538A317F1bC0Af88687AB37656D',
   marketplaceIdentifier:'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
--- smart-contract/lib/NftContractProvider.ts
+++ smart-contract/lib/NftContractProvider.ts
@@ -1,8 +1,8 @@
-// The name below ("PiDAOClub1") should match the name of your Solidity contract.
+// The name below ("PiAPEClub") should match the name of your Solidity contract.
 // It can be updated using the following command:
 // yarn rename-contract NEW_CONTRACT_NAME
 // Please DO NOT change it manually!
-import { PiDAOClub1 as ContractType } from '../typechain/index';
+import { PiAPEClub as ContractType } from '../typechain/index';
 
 import { ethers } from 'hardhat';
 import CollectionConfig from './../config/CollectionConfig';
